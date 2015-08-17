278616591fb6074d3f854890b489796c245beab5
TWer <twer@YXL.local>
2015-08-17 15:31:17 +0800

    APP

--- CalculatorApp/ViewController.h
+++ CalculatorApp/ViewController.h
@@ -10,6 +10,29 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UITextField *ResultText;
+
+enum CalType{ADD=10,SUB,MULT,DIV};
+
+- (IBAction)one:(id)sender;
+- (IBAction)two:(id)sender;
+- (IBAction)three:(id)sender;
+- (IBAction)four:(id)sender;
+- (IBAction)five:(id)sender;
+- (IBAction)six:(id)sender;
+- (IBAction)seven:(id)sender;
+- (IBAction)eight:(id)sender;
+- (IBAction)nine:(id)sender;
+- (IBAction)zero:(id)sender;
+
+
+- (IBAction)add:(id)sender;
+
+- (IBAction)sub:(id)sender;
+
+- (IBAction)result:(id)sender;
+
+- (IBAction)clear:(id)sender;
 
 @end
 
--- CalculatorApp/ViewController.m
+++ CalculatorApp/ViewController.m
@@ -10,12 +10,20 @@
 
 @interface ViewController ()
 
+
 @end
 
 @implementation ViewController
-
+NSMutableString *strregister;
+int ifirst;
+int isecond;
+int caltype;
 - (void)viewDidLoad {
     [super viewDidLoad];
+    strregister=[NSMutableString stringWithCapacity:10];
+   
+    self.ResultText.text=@"0";
+
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,5 +31,90 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+- (IBAction)one:(id)sender {
+     [strregister appendString:@"1"];
+    self.ResultText.text=strregister;
+}
+
+- (IBAction)two:(id)sender {
+    [strregister appendString:@"2"];
+    self.ResultText.text=strregister;
+}
+
+- (IBAction)three:(id)sender {
+    [strregister appendString:@"3"];
+    self.ResultText.text=strregister;
+}
+
+- (IBAction)four:(id)sender {
+    [strregister appendString:@"4"];
+    self.ResultText.text=strregister;
+}
+
+- (IBAction)five:(id)sender {
+    [strregister appendString:@"5"];
+    self.ResultText.text=strregister;
+}
+
+- (IBAction)six:(id)sender {
+    [strregister appendString:@"6"];
+    self.ResultText.text=strregister;
+}
+
+- (IBAction)seven:(id)sender {
+    [strregister appendString:@"7"];
+    self.ResultText.text=strregister;
+}
+
+- (IBAction)eight:(id)sender {
+    [strregister appendString:@"8"];
+    self.ResultText.text=strregister;
+}
+
+- (IBAction)nine:(id)sender {
+    [strregister appendString:@"9"];
+    self.ResultText.text=strregister;
+}
+
+- (IBAction)zero:(id)sender {
+    [strregister appendString:@"0"];
+    self.ResultText.text=strregister;
+}
+
+- (IBAction)add:(id)sender {
+    caltype=ADD;
+    ifirst=[strregister intValue];
+    [strregister setString:@""];
+}
+
+- (IBAction)sub:(id)sender {
+    [strregister appendString:@"-"];
+    self.ResultText.text=strregister;
+}
+
+- (IBAction)result:(id)sender {
+    
+    switch (caltype) {
+        case ADD:
+            isecond=[strregister intValue];
+            self.ResultText.text=[NSString stringWithFormat:@"%d",ifirst+isecond];
+            break;
+        case SUB:
+            isecond=[strregister intValue];
+            self.ResultText.text=[NSString stringWithFormat:@"%d",ifirst-isecond];
+            break;
+        default:
+            break;
+    }
+}
+
+- (IBAction)clear:(id)sender {
+    [strregister setString:@""];
+    self.ResultText.text=@"0";
+}
+
 
 @end
